6199985f7d333a37f19292bebc2652e2e34fde18
Sam Stern <hatboysam@gmail.com>
2019-12-25 06:59:59 -0800

    Set up tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <button 
+      id="test-btn"
+      class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+      Hello World
+    </button>
+
+    <HelloWorld msg="Welcome to Diff Machine"/>
   </div>
 </template>
 
