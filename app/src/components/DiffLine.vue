4fc502d7081db34bb953714030a312784b2229d9
Sam Stern <hatboysam@gmail.com>
2020-01-01 16:54:18 -0800

    Some cleanup

---
+++ app/src/components/DiffLine.vue
@@ -0,0 +1,55 @@
+<template>
+  <!-- TODO: Better key -->
+  <!-- TODO: I think each line should be a component -->
+  <p v-bind:class="classObject">
+    <code class="no-select pl-2">{{ lineNumber }}</code>
+    <code class="no-select px-2">{{ lineMarker }}</code>
+    <code>{{ lineContent }}</code>
+  </p>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class DiffLine extends Vue {
+  // TODO: types
+  @Prop() public change: any;
+
+  public get classObject(): object {
+    return {
+      "bg-gray-200": this.change.type === "normal",
+      "bg-red-200": this.change.type === "del",
+      "bg-green-200": this.change.type === "add"
+    };
+  }
+
+  public get lineMarker(): string {
+    if (this.change.type === "normal") {
+      return " ";
+    } else if (this.change.type === "del") {
+      return "-";
+    } else {
+      return "+";
+    }
+  }
+
+  public get lineContent(): string {
+    if (this.change.type === "normal") {
+      return this.change.content;
+    } else {
+      return this.change.content.substring(1);
+    }
+  }
+
+  public get lineNumber(): number {
+    return this.change.ln || this.change.ln1 || this.change.ln2;
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.no-select {
+  user-select: none;
+}
+</style>
--- app/src/components/TextDiff.vue
+++ app/src/components/TextDiff.vue
@@ -1,83 +1,51 @@
 <template>
   <div class="flex">
-    <div class="flex-auto">
-      <pre
+    <div class="flex-1">
+      <DiffLine
         v-bind:key="change.content"
-        v-for="change in left"
-        v-bind:class="lineClass(change)"
-        >{{ fullLine(change) }}</pre
-      >
+        v-for="change in leftChanges"
+        v-bind:change="change"
+      ></DiffLine>
     </div>
-    <div class="flex-auto">
-      <!-- TOOD: Better key -->
-      <pre
+    <div class="flex-1">
+      <DiffLine
         v-bind:key="change.content"
-        v-for="change in right"
-        v-bind:class="lineClass(change)"
-        >{{ fullLine(change) }}</pre
-      >
+        v-for="change in rightChanges"
+        v-bind:change="change"
+      ></DiffLine>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
+import DiffLine from "./DiffLine.vue";
 
-@Component
+@Component({
+  components: {
+    DiffLine
+  }
+})
 export default class TextDiff extends Vue {
   // TODO: Get data passed in
   public diff = require("../../data/diff.json")[0];
   // TODO: What does multiple chunks mean?
   public changes = this.diff.chunks[0].changes;
-  public left = this.changes.filter((c: any) => {
-    return c.type === "normal" || c.type === "del";
-  });
-  public right = this.changes.filter((c: any) => {
-    return c.type === "normal" || c.type === "add";
-  });
 
-  // TODO: Number and marker shoud not be selectable
-  fullLine(c: any): string {
-    return ` ${this.lineNumber(c)} ${this.lineMarker(c)} ${this.lineContent(
-      c
-    )}`;
+  public get leftChanges() {
+    return this.changes.filter((c: any) => {
+      return c.type === "normal" || c.type === "del";
+    });
   }
 
-  // TODO: this can't be the clean way to style by data...
-  lineClass(c: any): string {
-    if (c.type === "normal") {
-      return "bg-gray-200";
-    } else if (c.type === "del") {
-      return "bg-red-200";
-    } else {
-      return "bg-green-200";
-    }
-  }
-
-  lineMarker(c: any): string {
-    if (c.type === "normal") {
-      return "";
-    } else if (c.type === "del") {
-      return "-";
-    } else {
-      return "+";
-    }
-  }
-
-  lineContent(c: any): string {
-    if (c.type === "normal") {
-      return c.content;
-    } else {
-      return c.content.substring(1);
-    }
-  }
-
-  lineNumber(c: any): number {
-    return c.ln || c.ln1 || c.ln2;
+  public get rightChanges() {
+    return this.changes.filter((c: any) => {
+      return c.type === "normal" || c.type === "add";
+    });
   }
 }
 </script>
 
 <style scoped lang="scss">
-// TODO
+// None...
 </style>
